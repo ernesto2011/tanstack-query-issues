077077b8a3e2e95cfcdc3fad4744411955bd8020
ernesto2011 <skillmx107@gmail.com>
2025-03-11 21:47:19 -0600

    :rocket: iniciando curso de tankstack query

---
+++ src/issues/components/LabelPicker.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import { sleep } from "../../helpers/slepp";
+
+const getLabels= async():Promise<any>=>{
+  await sleep(3000)
+  const response = await fetch('https://api.github.com/repos/facebook/react/labels')
+  const data = await response.json()
+  return data
+
+}
+export const LabelPicker = () => {
+  const labelsQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels
+  })
+  if(labelsQuery.isLoading){
+    return (
+      <div className="flex items-center justify-center">
+        <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white"></div>
+      </div>
+    )
+  }
+  return (
+    <>
+      <span
+        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
+    </>
+  );
+};
